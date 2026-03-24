b92e80dc2ab46426e9f94843e3e63703b31d1044
Matthew <m07wang@gmail.com>
2026-03-23 21:29:07 -0500

    sync

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function showDropdown () {
+  const dropdown = document.querySelector('.dropdown');
+  dropdown.style.display = 'flex';
+}
+
+function hideDropdown() {
+  const dropdown = document.querySelector('.dropdown');
+  dropdown.style.display = 'none';
+}
